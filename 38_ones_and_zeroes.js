a46024f6a7749ccc0a692960eca9b6737ee2aba1
Jordan Morris <jordanandrewmorris1@gmail.com>
2019-07-02 10:53:03 +1000

    added 38

---
+++ 38_ones_and_zeroes.js
@@ -0,0 +1,13 @@
+//Ones and Zeroes
+/*Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+Examples:
+
+Testing: [0, 0, 0, 1] ==> 1
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 0, 1] ==> 5
+Testing: [1, 0, 0, 1] ==> 9
+Testing: [0, 0, 1, 0] ==> 2
+*/
