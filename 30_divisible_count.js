b25b4a9fe19422288fbb7854ba2135aa2736409f
Jordan Morris <jordanandrewmorris1@gmail.com>
2019-07-02 10:18:05 +1000

    remove from master

--- 30_divisible_count.js
+++
@@ -1,6 +0,0 @@
-/*Count the divisible numbers
-Problem Description: Write function divisibleCount(x, y, z) that takes in 3 numbers x, y and z, and returns the number
-of integers within the range [x..y] that are divisible by z i.e.: 
-For example:
-x = 6, y = 11 and z = 2, your function should return 3, because there are three numbers divisible by 2 within the 
-range [6..11], namely 6, 8 and 10.*/
