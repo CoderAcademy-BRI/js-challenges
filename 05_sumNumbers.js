6873120c08db97d19ddcfa899d9352c45a6727be
BlueCodeThree <carliemhamilton@gmail.com>
2019-09-04 11:03:48 +1000

    finished 5

--- 05_sumNumbers.js
+++ 05_sumNumbers.js
@@ -3,8 +3,22 @@
 // Test your solution:
 // npm test
 
+function calculateTotalScore(scores) {
+    // calculates the total of the array of scores tha is passed in
+    let totalScore = 0;
+    for (let score of scores) {
+        totalScore += score;
+    }
+    return totalScore;
+}
+
 function findWinner(players) {
-    // Your code here
+    let winner = players[0].name;
+    if ( calculateTotalScore(players[0].scores) < calculateTotalScore(players[1].scores)) {
+        winner = players[1].name;
+        return winner;
+    }
+    return winner;
 }
 
 module.exports = {
