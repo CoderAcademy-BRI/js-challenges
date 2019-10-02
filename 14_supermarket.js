b4adfe0b91f489cf66afc0b65764f424f95ded09
BlueCodeThree <carliemhamilton@gmail.com>
2019-10-02 22:23:18 +1000

    adds 14 to done!

--- 14_supermarket.js
+++ 14_supermarket.js
@@ -1,15 +1,15 @@
 /*
-There is a queue for the self-checkout tills at the supermarket. 
-Your task is write a function to calculate the total time required for 
+There is a queue for the self-checkout tills at the supermarket.
+Your task is write a function to calculate the total time required for
 all the customers to check out!
 
 The function has two input variables:
-    customers: an array of positive integers 
-            representing the queue. Each integer represents a customer, 
+    customers: an array of positive integers
+            representing the queue. Each integer represents a customer,
             and its value is the amount of time they require to check out.
     n: a positive integer, the number of checkout tills.
 
-The function should consider each person in order, and add them to the 
+The function should consider each person in order, and add them to the
 'smallest' line at that time (the line with the least total time).
 
 The function should return an integer, the time it takes for all lines to empty.
@@ -32,7 +32,17 @@ Check your solutions with npm test
 
 // Returns the queue time for all customers given number of tills
 function queueTime(customers, n) {
-    // Your code here
+    // so need to calculate when the time would run out, have a current sum for each till and see which is less, that would be the next till for the next person.
+    // how do we have a sum for each queue? For each till, create an array and then have that index be the queue and will see which index is currently the min, and that one is the one the thing will be added to.
+    // or, have an array for each queue and the lowest sum will show which queue
+    // lets do it with one till first
+    let queues = Array.apply(null, Array(n)).map(function () {return 0;});
+    for (let customer of customers) {
+        // first check which is the lowest number in queues
+        let minIndex = queues.indexOf(Math.min(...queues));
+        queues[queues.indexOf(Math.min(...queues))] += customer;
+    }
+    return Math.max(...queues);
 }
 
 module.exports = {
