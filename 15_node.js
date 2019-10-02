7f27d9bad70c3506ef8b8c4bba03b5e1121b887b
BlueCodeThree <carliemhamilton@gmail.com>
2019-10-03 07:52:23 +1000

    completes 15

--- 15_node.js
+++ 15_node.js
@@ -31,6 +31,12 @@ const requestHandler = (request, response) => {
     response.end('world')
   }
 
+  if (request.url === '/') {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write("<h1>Hello</h1>");
+    response.end("Carlie's awesome")
+  }
+
   response.end('Not found!!')
 }
 
