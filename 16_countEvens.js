4443f343ce27a29ee5e1a82574e2d6cc91f4e8a3
BlueCodeThree <carliemhamilton@gmail.com>
2019-10-08 08:53:49 +1000

    16 is a wip

--- 16_countEvens.js
+++ 16_countEvens.js
@@ -1,12 +1,12 @@
 /*
 
 Working individually or in pairs write out differnt tests which will
-count the number of even numbers in an array. 
+count the number of even numbers in an array.
 
 Try to have at least three different tests which test differnt values
 put into your method, such as an empty array.
 
-Once you have done done this, write out the method and check if it passes. 
+Once you have done done this, write out the method and check if it passes.
 
 Example: countEvens = ([1,2,3,4]) => 2
 
@@ -17,7 +17,39 @@ Google - mocha tests to get an idea.
 */
 
 const countEvens = (arr) => {
-// Your code here
+    let count = 0;
+    if (arr.count < 0) { return 0;};
+    for (let num in arr) {
+        if (num % 2 == 0) {
+            count++;
+        };
+    };
+    return count;
 }
-  
+
 // Your tests here
+var assert = require("assert")
+
+describe('countEvens', function() {
+    describe('Should count 1 even number', function () {
+        test("should return 1 with [1,2,3]", function () {
+            expect(countEvens([1,2,3])).toBe(1);
+        });
+    });
+    describe('Should count < 1 even numbers', function () {
+        test("should return 2 with [1,2,3,4]", function () {
+            expect(countEvens([1,2,3,4])).toBe(2);
+        });
+        test("should return 3 with [2,3,4,5,6]", function() {
+            expect([2,3,4,5,6]).toBe(3);
+        });
+    });
+    describe('Should count 0 with no even numbers', function () {
+        test("should return 0 with empty array", function () {
+            expect([]).toBe(0);
+        });
+        test('should be 0 with [1,3,5]', function() {
+            expect([1,3,5]).toBe(0);
+        });
+    });
+});
